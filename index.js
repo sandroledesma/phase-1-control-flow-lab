71d2b1fdd83ac6d3e5091042aa739cb96c6e85c5
Alessandro <sandroledesma@gmail.com>
2024-04-27 16:54:55 -0400

    update files

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  let response;
+  if (distance <= 400) {
+    response = 'This one is on me!';
+  } else if (distance > 400 && distance <= 2000) {
+    response = 'That will be twenty bucks.';
+  } else if (distance > 2000 && distance <= 2500) {
+    response = 'I will gladly take your thirty bucks.';
+  } else if (distance > 2500) { 
+    response = 'No can do.';
+  }
+
+  return response;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return (city === 'NYC') ? 'Ok, sounds good.' : 'No go.';
+  
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(percent){
   // Write your code here!
+  let tipResponse;
+
+  switch(percent) {
+    case 'generous':
+      tipResponse = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      tipResponse = 'Thank you.';
+      break;
+    default:
+      tipResponse = 'Bye.';
+      break;
+
+  }
+  return tipResponse;
 }
